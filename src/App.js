3ee84a57df7e458b4e900e479c38828985fa8f8a
fa3ze <118280320+fa3ze@users.noreply.github.com>
2022-12-27 04:34:30 +0330

    linked github

--- src/App.js
+++ src/App.js
@@ -4,7 +4,26 @@ function App() {
   return (
     <div className="App">
       Weather App
-      <a href=""></a>
+      <div className="references">
+        This project was coded by{" "}
+        <a
+          href="https://www.linkedin.com/in/faeze-azimi-50a931255/"
+          alt="faeze linkedin"
+          rel="noreferrer"
+          target="_blank"
+        >
+          Faeze Azimi
+        </a>{" "}
+        and is open sourced on
+        <a
+          href="https://github.com/fa3ze/Faeze-Weather-App"
+          alt="github"
+          rel="noreferrer"
+          target="_blank"
+        >
+          github
+        </a>
+      </div>
     </div>
   );
 }
