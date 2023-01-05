980076960479c20bfa3b190dfe933c8675f48cb9
fa3ze <118280320+fa3ze@users.noreply.github.com>
2023-01-05 10:47:35 +0330

    fixed sunrise sunset time format

--- src/MoreInfo.js
+++ src/MoreInfo.js
@@ -1,7 +1,5 @@
-import React, { useState } from "react";
+import React from "react";
 import "./Weather.css";
-
-//import WeatherForecastIcon from "./WeatherForecastIcon";
 import windIcon from "./picture/wind.png";
 import uvIcon from "./picture/uvi.png";
 import sunriseIcon from "./picture/sunrise.png";
@@ -9,6 +7,22 @@ import sunsetIcon from "./picture/sunset.png";
 import percipitationIcon from "./picture/percipitation.png";
 import cloudy from "./picture/cloudy.png";
 export default function Info(props) {
+  function getTime(timestamp, timeZone) {
+    let date = new Date(timestamp);
+    let hours = Math.round((date.getUTCHours() + timeZone + 24) % 24);
+    if (hours < 10) {
+      hours = `0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes < 10) {
+      minutes = `0${minutes}`;
+    }
+    return `${hours}:${minutes}`;
+  }
+
+  let sunriseFormatTime = getTime(props.data.sunriseTime, props.data.timezone);
+  let sunsetFormatTime = getTime(props.data.sunsetTime, props.data.timezone);
+
   return (
     <div className="container show-more-box">
       <div className="row mt-4">
@@ -84,7 +98,7 @@ export default function Info(props) {
             <img src={sunriseIcon} alt="sunriseIcon" className="img-fluid" />
           </div>{" "}
           <div className="row">
-            <div className=" text-center"> {props.data.sunriseTime}</div>
+            <div className=" text-center"> {sunriseFormatTime}</div>
           </div>
         </div>
         <div className="col-2 forecastBox">
@@ -98,7 +112,7 @@ export default function Info(props) {
             <img src={sunsetIcon} alt="sunsetIcon" className="img-fluid" />
           </div>{" "}
           <div className="row">
-            <div className="daytemp text-center"> {props.data.sunsetTime}</div>
+            <div className="daytemp text-center">{sunsetFormatTime}</div>
           </div>
         </div>
       </div>
